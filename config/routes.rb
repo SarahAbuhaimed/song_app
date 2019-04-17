dc23d79b25a9995bede02644e5d7edf86382aeee
mohammed <alhammad2013@gmail.com>
2019-04-17 15:22:10 +0300

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'songs/index'
-  get 'songs/show'
-
-
-  get 'albums/index'
-  get 'albums/show'
-
-  get 'artists/index'
-  get 'artists/show'
+  resources :songs
+  resources :artists
+  resources :albums
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
